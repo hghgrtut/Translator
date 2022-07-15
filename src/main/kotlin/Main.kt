fbba7c494ced99705937c43a3e2c16570f145ecd
Hg Hg <hghghghg.hh39@gmail.com>
2022-07-15 14:29:10 +0300

    minor improvements

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,11 @@ import java.io.File
 import java.io.FileInputStream
 import org.apache.poi.ss.usermodel.WorkbookFactory
 
-private const val SHEET_INDEX = 1
-private const val ROWS_COUNT = 84
+private const val SHEET_NAME = "Депозит"
+private const val ROWS_COUNT = 500
 private const val BY_TRANSLATION_COLUMN = 11
 private const val ENG_TRANSLATION_COLUMN = 8
-private const val RUS_TRANSLATION_COLUMN = 5
+private const val RUS_TRANSLATION_COLUMN = 4
 private const val EXCEL_FILE_NAME = "./Переводы Insync.xlsx"
 private const val INPUT_FILE_NAME = "./in.txt"
 private const val BY_FILE_NAME = "./by.txt"
@@ -15,12 +15,15 @@ private const val ENG_FILE_NAME = "./en.txt"
 fun main() {
     File(BY_FILE_NAME).delete()
     File(ENG_FILE_NAME).delete()
-    val excelSheet = WorkbookFactory.create(FileInputStream(EXCEL_FILE_NAME)).getSheetAt(SHEET_INDEX)
-    val map = mutableMapOf<String, Pair<String, String>>()
+
+
+    val excelSheet = WorkbookFactory.create(FileInputStream(EXCEL_FILE_NAME)).getSheet(SHEET_NAME)
+    val map = mutableMapOf<String, Pair<String?, String?>>()
     for (rowIndex in 0..ROWS_COUNT) {
         val row = excelSheet.getRow(rowIndex)
-        map[row.getCell(RUS_TRANSLATION_COLUMN).toString()] =
-            row.getCell(BY_TRANSLATION_COLUMN).toString() to row.getCell(ENG_TRANSLATION_COLUMN).toString()
+        val rusTranslation = row?.getCell(RUS_TRANSLATION_COLUMN).toString()
+        if (rusTranslation != "null") map[rusTranslation] = row.getCell(BY_TRANSLATION_COLUMN)?.toString() to
+                    row.getCell(ENG_TRANSLATION_COLUMN)?.toString()?.replace(Regex("[\'`]"), "\\\'")
     }
     File(INPUT_FILE_NAME).forEachLine {
         val key = it.substringAfter(">").substringBefore("<")
